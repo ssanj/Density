d7b98d97ebbb0696bc3b7f562e57eb6c2c1fc47a
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-09-05 12:39:00 +1000

    added test for moving progress bar sliders and ensuring the screensize is updated

--- test/src/main/java/com/robodreamz/density/test/DensityAppRobTest.java
+++ test/src/main/java/com/robodreamz/density/test/DensityAppRobTest.java
@@ -5,8 +5,11 @@
 package com.robodreamz.density.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ProgressBar;
 import com.jayway.android.robotium.solo.Solo;
 import com.robodreamz.density.DensityAppActivity;
+
+import java.util.ArrayList;
 
 public final class DensityAppRobTest extends ActivityInstrumentationTestCase2<DensityAppActivity> {
 
@@ -25,5 +28,14 @@ public final class DensityAppRobTest extends ActivityInstrumentationTestCase2<De
         assertTrue("DensityAppActivity did not show up", solo.waitForActivity("DensityAppActivity", 2000));
         assertTrue("Did not find default screen size", solo.searchText("2.5"));
         assertTrue("Did not find screen size unit", solo.searchText("\""));
+    }
+
+    public void testShouldUpdateScreenSizeOnMovingSliders() {
+        assertTrue("DensityAppActivity did not show up", solo.waitForActivity("DensityAppActivity", 2000));
+        final ArrayList<ProgressBar> currentProgressBars = solo.getCurrentProgressBars();
+        assertEquals("Incorrect number of progress bars on screen", 2, currentProgressBars.size());
+        solo.setProgressBar(currentProgressBars.get(0), 5);
+        solo.setProgressBar(currentProgressBars.get(1), 4);
+        assertTrue("Did not find expected screen size of 7.4", solo.searchText("7.4"));
     }
 }
