d21d2a07744aaa959ccc65ec72559cd8879d0806
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-09-08 12:24:50 +1000

    refactored Calcualtor

--- app/src/main/java/com/robodreamz/density/calc/DensityCalculator.java
+++ app/src/main/java/com/robodreamz/density/calc/DensityCalculator.java
@@ -7,22 +7,89 @@ package com.robodreamz.density.calc;
 public final class DensityCalculator {
 
     public static final int ERROR_VALUE = -9999;
-    private static final int MAX_RESOLUTION_SIZE = Integer.MAX_VALUE;
-    private static final double MAX_SCREEN_DIAGONAL_SIZE = 10000.0d;
+    public static final String NONE = "NONE";
+    //We don't really know how big resolutions will go. Integer.MAX_VALUE is a decent guess.
+    static final int MAX_RESOLUTION_SIZE = Integer.MAX_VALUE;
+    //Given that we have max screen sizes of around 10-12", a x1000 should be enough.
+    static final double MAX_SCREEN_DIAGONAL_SIZE = 10000.0d;
+    static final String WIDTH = "Width";
+    static final String HEIGHT = "Height";
 
-    public int getDensityFor(final int width, final int height, final double screenLength) {
-        if (invalidInteger(width) || invalidInteger(height) || invalidDouble(screenLength)) {
-            return ERROR_VALUE;
+    public DensityCaluclation getDensityFor(final int width, final int height, final double screenLength) {
+        final ValidationResult widthValidation = validateInteger(WIDTH, width);
+        final ValidationResult heightValidation = validateInteger(HEIGHT, height);
+        final ValidationResult screenValidation = validateDouble(screenLength);
+
+        if (widthValidation.isInvalid() || heightValidation.isInvalid() || screenValidation.isInvalid()) {
+            return new DensityCaluclation(widthValidation.isInvalid() ? widthValidation.reason
+                    : heightValidation.isInvalid() ? heightValidation.reason
+                    : screenValidation.reason);
         } else {
-            return (int) Math.round(Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2)) / screenLength);
+            return new DensityCaluclation((int) Math.round(Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2)) / screenLength));
         }
     }
 
-    private boolean invalidDouble(final double screenLength) {
-        return screenLength <= 0 || screenLength >= MAX_SCREEN_DIAGONAL_SIZE;
+    private ValidationResult validateDouble(final double screenLength) {
+        final boolean invalid = screenLength <= 0 || screenLength >= MAX_SCREEN_DIAGONAL_SIZE;
+        if (invalid) {
+            return new ValidationResult(screenLength <= 0 ? "Screen diagonal is less than 1" :
+                    "Screen diagonal is greater than max value: " + MAX_SCREEN_DIAGONAL_SIZE);
+        } else {
+            return new ValidationResult();
+        }
     }
 
-    private boolean invalidInteger(final int size) {
-        return size <= 0 || size >= MAX_RESOLUTION_SIZE;
+    private ValidationResult validateInteger(String attribute, final int size) {
+        final boolean invalid = size <= 0 || size >= MAX_RESOLUTION_SIZE;
+        if (invalid) {
+            return new ValidationResult(size <= 0 ? (attribute + " is less than 1") :
+                    (attribute + " is greater than max resolution size: " + MAX_RESOLUTION_SIZE));
+        } else {
+            return new ValidationResult();
+        }
+    }
+
+    public static final class DensityCaluclation {
+        private final int result;
+        private final String error;
+
+
+        public DensityCaluclation(final int result) {
+            this.result = result;
+            error = NONE;
+        }
+
+        public DensityCaluclation(final String error) {
+            this.error = error;
+            result = ERROR_VALUE;
+        }
+
+        public boolean isValid() {
+            return result != ERROR_VALUE;
+        }
+
+        public int getResult() {
+            return result;
+        }
+
+        public String getError() {
+            return error;
+        }
+    }
+
+    private static final class ValidationResult {
+        private final String reason;
+
+        private ValidationResult() {
+            reason = NONE;
+        }
+
+        private ValidationResult(final String reason) {
+            this.reason = reason;
+        }
+
+        public boolean isInvalid() {
+            return !reason.equals(NONE);
+        }
     }
 }
--- app/src/test/java/com/robodreamz/density/calc/DensityCalculatorTest.java
+++ app/src/test/java/com/robodreamz/density/calc/DensityCalculatorTest.java
@@ -7,7 +7,14 @@ package com.robodreamz.density.calc;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.robodreamz.density.calc.DensityCalculator.HEIGHT;
+import static com.robodreamz.density.calc.DensityCalculator.MAX_RESOLUTION_SIZE;
+import static com.robodreamz.density.calc.DensityCalculator.MAX_SCREEN_DIAGONAL_SIZE;
+import static com.robodreamz.density.calc.DensityCalculator.WIDTH;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public final class DensityCalculatorTest {
 
@@ -18,32 +25,57 @@ public final class DensityCalculatorTest {
     }
 
     @Test public void shouldCalculateTheDensityGivenAWidthAAHeightAndAScreenDiagonalLength() {
-        assertEquals("Incorrect density value", 102, calc.getDensityFor(100, 200, 2.2));
+        assertValidResult(calc.getDensityFor(100, 200, 2.2), 102);
     }
 
     @Test public void shouldCalculateTheDensityGivenAnotherWidthAAHeightAndAScreenDiagonalLength() {
-        assertEquals("Incorrect density value", 125, calc.getDensityFor(320, 240, 3.2));
+        assertValidResult(calc.getDensityFor(320, 240, 3.2), 125);
     }
 
     @Test public void shouldReturnAnErrorneousValueforZero() {
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(0, 240, 3.2));
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(320, 0, 3.2));
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(320, 240, 0));
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(0, 0, 0));
+        assertInvalidZeroAndLessThanZeroValue(calc.getDensityFor(0, 240, 3.2), "Width");
+        assertInvalidZeroAndLessThanZeroValue(calc.getDensityFor(320, 0, 3.2), "Height");
+        assertInvalidZeroAndLessThanZeroValue(calc.getDensityFor(320, 240, 0), "Screen diagonal");
+        assertInvalidZeroAndLessThanZeroValue(calc.getDensityFor(0, 0, 0), "Width");
     }
 
     @Test public void shouldReturnErrorneousValueForNegativeNumbers() {
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(-1, 240, 3.2));
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(320, -2, 3.2));
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(320, 240, -3.2));
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(-3, -1, -0));
+        assertInvalidZeroAndLessThanZeroValue(calc.getDensityFor(-1, 240, 3.2), "Width");
+        assertInvalidZeroAndLessThanZeroValue(calc.getDensityFor(320, -2, 3.2), "Height");
+        assertInvalidZeroAndLessThanZeroValue(calc.getDensityFor(320, 240, -3.2), "Screen diagonal");
+        assertInvalidZeroAndLessThanZeroValue(calc.getDensityFor(-3, -1, -0), "Width");
     }
 
     @Test public void shouldReturnErrorneousValueForMaxRanges() {
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(Integer.MAX_VALUE, 240, 3.2));
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(320, Integer.MAX_VALUE, 3.2));
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(320, 240, Double.MAX_VALUE));
-        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, calc.getDensityFor(Integer.MAX_VALUE, Integer.MAX_VALUE,
-                Double.MAX_VALUE));
+        assertInvalidMaxValue(calc.getDensityFor(Integer.MAX_VALUE, 240, 3.2), WIDTH);
+        assertInvalidMaxValue(calc.getDensityFor(320, Integer.MAX_VALUE, 3.2), HEIGHT);
+        assertInvalidMaxValueForScreenSize(calc.getDensityFor(320, 240, Double.MAX_VALUE));
+        assertInvalidMaxValue(calc.getDensityFor(Integer.MAX_VALUE, Integer.MAX_VALUE, Double.MAX_VALUE), WIDTH);
+    }
+
+    private void assertInvalidMaxValue(final DensityCalculator.DensityCaluclation result, final String attribute) {
+        assertFalse("Density result should be invalid", result.isValid());
+        assertEquals("Incorrect error String", attribute + " is greater than max resolution size: " + MAX_RESOLUTION_SIZE, result.getError());
+        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, result.getResult());
+    }
+
+    private void assertInvalidMaxValueForScreenSize(final DensityCalculator.DensityCaluclation result) {
+        assertFalse("Density result should be invalid", result.isValid());
+        assertEquals("Incorrect error String", "Screen diagonal is greater than max value: " + MAX_SCREEN_DIAGONAL_SIZE, result.getError());
+        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, result.getResult());
+    }
+
+    //TODO: Add test for verifying the first error is the error returned.
+
+    private void assertValidResult(final DensityCalculator.DensityCaluclation result, final int value) {
+        assertNotNull("Density result is null", result);
+        assertTrue("Density result should be valid", result.isValid());
+        assertEquals("Density result should be valid", value, result.getResult());
+    }
+
+    private void assertInvalidZeroAndLessThanZeroValue(final DensityCalculator.DensityCaluclation result, final String attribute) {
+        assertFalse("Density result should be invalid", result.isValid());
+        assertEquals("Incorrect error String", attribute + " is less than 1", result.getError());
+        assertEquals("Incorrect density value", DensityCalculator.ERROR_VALUE, result.getResult());
     }
 }
