72f0cf4edd5dfd07680fbb342839e2de8eaaf0ca
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-09-08 12:45:39 +1000

    added javadoc

--- app/src/main/java/com/robodreamz/density/calc/DensityCalculator.java
+++ app/src/main/java/com/robodreamz/density/calc/DensityCalculator.java
@@ -4,6 +4,15 @@
  */
 package com.robodreamz.density.calc;
 
+/**
+ * The calculations for screen density are done here. We use the following formula for calculations:
+ * SQR(SQ(WIDTH) + SQ(HEIGHT)) / SCREEN_DIAGONAL_LENGTH.
+ * <p/>
+ * We try not to throw Exceptions or return null but instead return the results as objects that must be queried for its state.
+ * <p/>
+ * Always call  <code>DensityCaluclation#isValid</code> to learn the current state of the result.
+ * @see  DensityCaluclation
+ */
 public final class DensityCalculator {
 
     public static final int ERROR_VALUE = -9999;
@@ -18,6 +27,15 @@ public final class DensityCalculator {
     static final String HEIGHT = "Height";
     static final String SCREEN_DIAGONAL = "Screen diagonal";
 
+    /**
+         * Calculates the screen resolution given the width, height and screen length. The width and height can be transposed as this does not affect the calculated value.
+         * @param width The resolution width.
+         * @param height The resolution heght.
+         * @param screenLength The length of the screen diagonal.
+         * @return DensityCaluclation with the result. If in error, <code>DensityCaluclation#isValid</code> returns false. In this case call <code>DensityCaluclation#getError</code> to
+         * display some manner of error to the user. If not in error call <code>DensityCaluclation#getResult</code> to retrieve the result.
+         * @see DensityCaluclation
+     */
     public DensityCaluclation getDensityFor(final int width, final int height, final double screenLength) {
         final ValidationResult widthValidation = validateInteger(WIDTH, width);
         final ValidationResult heightValidation = validateInteger(HEIGHT, height);
@@ -52,6 +70,14 @@ public final class DensityCalculator {
         }
     }
 
+    /**
+         * Wrapper class for a calculation result. Always call <code>isValid</code> before using the result. If this object return false from <code>isValid</code> then the error that
+         * occurred can be extracted through the <code>getError</code> method. In this state <code>getResult</code> will return a value of
+         * <code>DensityCalculator#ERROR_VALUE</code>.
+         * <p/>
+         * If this object is valid then the result can be retrieved through <code>getResult</code>. Calling <code>getError</code> in this state will return the value
+         *   of <code>DensityCalculator#NONE</code>.
+     */
     public static final class DensityCaluclation {
         private final int result;
         private final String error;
