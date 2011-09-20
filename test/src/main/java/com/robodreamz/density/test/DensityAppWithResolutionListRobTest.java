91fddd4f82edc13b7b8d638131f1a372e934bf8a
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-09-20 16:09:03 +1000

    fixed broked tests by circumventing problem

--- test/src/main/java/com/robodreamz/density/test/DensityAppWithResolutionListRobTest.java
+++ test/src/main/java/com/robodreamz/density/test/DensityAppWithResolutionListRobTest.java
@@ -14,11 +14,13 @@ public final class DensityAppWithResolutionListRobTest extends AbstractDensityAp
 
     public void testResolutionsAreDisplayed() {
         waitForApplicationActivity();
+        assertTrue("Did not find header text", solo.waitForText("Common", 1, 2000, true));
+
         final List<ListView> currentListViews = solo.getCurrentListViews();
         assertEquals("The number of ListsViews on the screen is incorrect", 1, currentListViews.size());
         assertTrue("The number of resolutions is incorrect", currentListViews.get(0).getCount() > 1);
         final List<TextView> headers = solo.clickInList(0, 0);
-        assertEquals("Incorrect number of header data", 1, headers.size());
+        assertEquals("Incorrect number of header data", 2, headers.size());
         assertEquals("Incorrect header text", getActivity().getResources().getString(R.string.resolution_list_header_element_title), headers.get(0).getText());
     }
 }
